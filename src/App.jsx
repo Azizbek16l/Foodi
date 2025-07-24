76a1a675ca2950d5904bd4acdcdf6da32b6f1409
Azizbek16l <azizbektopilboyev7@gmail.com>
2025-07-24 15:36:20 +0500

    faylar qoshildi

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+     <header>
+      <div className="container"></div>
+     </header> 
+    <main>
+      <div className="container"></div>      
+    </main>
+    <section>
+      <div className="container"></div>
+    </section>
+    <section>
+      <div className="container"></div>      
+    </section>
+    <section>
+      <div className="container"></div>
+    </section>
+    <section>
+      <div className="container"></div>
+    </section>
+    <article>
+      <div className="contianer"></div>
+    </article>
+    <footer>
+      <div className="container"></div>
+    </footer>
+    </>
+  )
+}
+
+export default App
